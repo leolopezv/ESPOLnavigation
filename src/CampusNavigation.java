1575a4057d77204326969e73d9884ee10c378855
leolopezv <leonardo.lopez@uees.edu.ec>
2024-12-01 15:14:16 -0500

    Creation of .txt files

--- src/CampusNavigation.java
+++ src/CampusNavigation.java
@@ -9,7 +9,7 @@ public class CampusNavigation {
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
         dijkstra.computeShortestPaths(source);
 
-        System.out.println("Rutas más convenientes desde " + graph.getNodeName(source) + ":");
+        System.out.println("Ruta más convenientes desde " + graph.getNodeName(source) + ":");
         for (int destination : graph.getNodes().keySet()) {
             if (destination != source) {
                 System.out.print("Camino a " + graph.getNodeName(destination) + ": ");
@@ -33,11 +33,11 @@ public class CampusNavigation {
     private static Graph getGraph() {
         Graph graph = new Graph();
 
-        graph.addNode(0, "Facultad de Ingeniería en Electricidad y Computación");
-        graph.addNode(1, "Biblioteca Central");
-        graph.addNode(2, "Cafetería");
-        graph.addNode(3, "Auditorio Principal");
-        graph.addNode(4, "Laboratorios de Química");
+        graph.addNode(0, "FCSH");
+        graph.addNode(1, "FCNM");
+        graph.addNode(2, "FIMCP");
+        graph.addNode(3, "SWEET");
+        graph.addNode(4, "DOBE");
 
         graph.addEdge(0, 1, 500, 1.5);
         graph.addEdge(0, 2, 300, 1.0);
