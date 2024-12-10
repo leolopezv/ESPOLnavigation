2754d15b2d2a0143c6f6e779d95a6dd8293f7599
leolopezv <leonardo.lopez@uees.edu.ec>
2024-12-10 16:12:24 -0500

    Final Presentation

--- src/CampusApp.java
+++ src/CampusApp.java
@@ -2,6 +2,7 @@ import service.CampusNavigator;
 
 public class CampusApp {
 
+    //By Andrea del Pino & Leonardo Lopez
     public static void main(String[] args) {
         CampusNavigator navigator = new CampusNavigator();
         navigator.run();
